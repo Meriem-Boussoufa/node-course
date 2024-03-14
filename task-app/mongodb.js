60ed9daa3bca1e4e020ce1063bd7320483f2a102
meriem <meriem@gmail.com>
2024-03-14 14:14:19 +0100

    objectID

--- task-app/mongodb.js
+++ task-app/mongodb.js
@@ -1,31 +1,36 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 
 const dbName = 'task-app';
 
+const id = new ObjectId()
+console.log(id)
+console.log(id.getTimestamp())
+console.log(id.id.length)
+
 async function main() {
   await client.connect();
   console.log('Connected successfully to server');
   const db = client.db(dbName);
-  const collection = db.collection('tasks');
+//   const collection = db.collection('tasks');
 
-  const insertResult = await collection.insertMany([
-    { 
-        description: 'Clean the house',
-        completed: true,
-    },
-    { 
-        description: 'Renew inspection',
-        completed: false,
-    },
-    { 
-        description: 'Pot plants',
-        completed: false,
-    },
-]);
-  console.log('Inserted documents =>', insertResult);
+//   const insertResult = await collection.insertMany([
+//     { 
+//         description: 'Clean the house',
+//         completed: true,
+//     },
+//     { 
+//         description: 'Renew inspection',
+//         completed: false,
+//     },
+//     { 
+//         description: 'Pot plants',
+//         completed: false,
+//     },
+// ]);
+//   console.log('Inserted documents =>', insertResult);
 
   return 'done.';
 }
