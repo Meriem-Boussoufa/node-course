d5d410524dff7ad2c6d236b29adf0278ce7b4c7b
meriem <meriem@gmail.com>
2024-03-15 14:14:34 +0100

    data validation

--- task-app/src/db/mongoose.js
+++ task-app/src/db/mongoose.js
@@ -1,18 +1,50 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+const mongoose = require('mongoose')
+const validator = require('validator')
+mongoose.connect('mongodb://127.0.0.1:27017/test')
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim:true,
+    },
+    email : {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password : {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if(value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
 const me = new User({
-    name: 'Meriem',
-    age: 24
+    name: 'Test',
+    email: 'meriem@gmail.com',
+    password: 'hello123456789'
 })
 
 me.save().then(() => {
@@ -24,16 +56,18 @@ me.save().then(() => {
 
 const Task = mongoose.model('Task', {
     description: {
-        type : String
+        type : String,
+        required: true,
+        trim: true,
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false,
     }
 })
 
 const task =  new Task({
-    description: 'Learn the Mongoose library',
-    completed: false
+    description: ' Trim Test   ',
 })
 
 task.save().then(() => {
