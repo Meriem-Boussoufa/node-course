d6aa0b614c0be5cac907606d113fb8d8240923c7
meriem <meriem@gmail.com>
2024-03-15 15:14:03 +0100

    integrate async/await in /users post

--- task-app/src/index.js
+++ task-app/src/index.js
@@ -8,14 +8,20 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
-
-    user.save().then(() => {
+    try {
+        await user.save()
         res.status(201).send(user)
-    }).catch((e) => {
+    }catch(e) {
         res.status(400).send(e)
-    })
+    }
+
+    // user.save().then(() => {
+    //     res.status(201).send(user)
+    // }).catch((e) => {
+    //     res.status(400).send(e)
+    // })
 })
 
 app.post('/tasks', (req, res) => {
