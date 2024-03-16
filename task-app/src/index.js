d1b3c913e97c9ccb305aca87191c8ed790b2ab39
meriem <meriem@gmail.com>
2024-03-16 15:09:21 +0100

    express middleware

--- task-app/src/index.js
+++ task-app/src/index.js
@@ -8,8 +8,26 @@ const userRouter = require('./routers/user')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if(req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     }else {
+//         next()
+//     }
+// })
+
+app.use((req, res, next) => {
+    res.status(503).send('Site is currently down. Check back soon!')
+})
+
 app.use(express.json())
 app.use(userRouter)
+
+//
+// Without middleware: new request --> run route handler
+//
+// with middleware: new request --> do something --> run route handler
+//
 
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
