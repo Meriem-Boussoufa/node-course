5382fb9c23680fab815c6643435cd15aa95ee58a
meriem <meriem@gmail.com>
2024-03-16 16:02:24 +0100

    the user task relation

--- task-app/src/index.js
+++ task-app/src/index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 require('./db/mongoose')
 
-const Task = require('./models/task')
-
 const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -22,6 +21,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(userRouter)
+app.use(taskRouter)
 
 //
 // Without middleware: new request --> run route handler
@@ -29,75 +29,21 @@ app.use(userRouter)
 // with middleware: new request --> do something --> run route handler
 //
 
-app.post('/tasks', (req, res) => {
-    const task = new Task(req.body)
-
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
-})
-
-app.get('/tasks', (req,res) => {
-    Task.find({}).then((tasks) => {
-        res.send(tasks)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-app.get('/tasks/:id', (req,res) => {
-    const _id = req.params.id
-    Task.findById(_id).then((task) => {
-        if(!task) {
-            return res.status(404).send()
-        }
-        res.send(task)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-
-app.patch('/tasks/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['description', 'completed']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
-
-    try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
-
-        if (!task) {
-            return res.status(404).send()
-        }
-
-        res.send(task)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.delete('/tasks/:id', async (req, res) => {
-    try {
-        const task = await Task.findByIdAndDelete(req.params.id)
-
-        if (!task) {
-            res.status(404).send()
-        }
-
-        res.send(task)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+    // const task = await Task.findById('')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+    const user = await User.findById('')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks)
+}
+
+main()
